5a21c144a277782e463598f4886d07cd8c36bd38
Alexey <sisipun@gmail.com>
2018-11-04 13:19:06 +0300

    score

--- core/src/io/cucumber/constant/HeroConstants.kt
+++ core/src/io/cucumber/constant/HeroConstants.kt
@@ -5,7 +5,7 @@ object HeroConstants {
     const val HORIZONTAL_VELOCITY = 10f
     const val VERTICAL_VELOCITY = -400f
 
-    const val WIDTH = 50f
-    const val HEIGHT = 50f
+    const val HERO_WIDTH = 50f
+    const val HERO_HEIGHT = 50f
 
 }
--- core/src/io/cucumber/view/GameScreen.kt
+++ core/src/io/cucumber/view/GameScreen.kt
@@ -4,10 +4,13 @@ import com.badlogic.gdx.Game
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.Input.Keys.LEFT
 import com.badlogic.gdx.Input.Keys.RIGHT
-import io.cucumber.constant.HeroConstants
+import io.cucumber.constant.HeroConstants.HERO_HEIGHT
+import io.cucumber.constant.HeroConstants.HERO_WIDTH
 import io.cucumber.constant.HeroConstants.HORIZONTAL_VELOCITY
 import io.cucumber.constant.HeroConstants.VERTICAL_VELOCITY
-import io.cucumber.constant.ScreenConstants
+import io.cucumber.constant.ScreenConstants.ENEMY_RESPAWN_BORDER
+import io.cucumber.constant.ScreenConstants.SCREEN_HEIGHT
+import io.cucumber.constant.ScreenConstants.SCREEN_WIDTH
 import io.cucumber.factory.EnemyGroupFactory
 import io.cucumber.factory.EnemyGroupFactory.GroupType.SIMPLE
 import io.cucumber.model.EnemyGroup
@@ -19,19 +22,21 @@ class GameScreen(
 ) : BaseScreen(game) {
 
     private val hero: Hero = Hero(
-        ScreenConstants.SCREEN_WIDTH / 2,
-        ScreenConstants.SCREEN_HEIGHT / 2,
-        HeroConstants.WIDTH,
-        HeroConstants.HEIGHT,
+        SCREEN_WIDTH / 2,
+        SCREEN_HEIGHT / 2,
+        HERO_WIDTH,
+        HERO_HEIGHT,
         HORIZONTAL_VELOCITY,
         VERTICAL_VELOCITY
     )
     private var enemyGroup: EnemyGroup = EnemyGroupFactory.create(SIMPLE)
+    private var score: Int = 0
 
 
     override fun update(delta: Float) {
         hero.update(delta)
         enemyGroup.update(delta)
+        score += (delta * 1000).toInt()
     }
 
     override fun render() {
@@ -54,15 +59,14 @@ class GameScreen(
     }
 
     override fun handleInput() {
-        if (hero.bound.y + hero.bound.height >= camera.position.y + (ScreenConstants.SCREEN_HEIGHT / 2)) {
+        if (hero.bound.y + hero.bound.height >= camera.position.y + (SCREEN_HEIGHT / 2)) {
             hero.direction = Hero.Direction.DOWN
         } else if (hero.bound.y <= 0) {
             hero.direction = Hero.Direction.UP
-
         }
 
         if (Gdx.input.isKeyPressed(LEFT) && hero.bound.x > 0) hero.moveLeft()
-        if (Gdx.input.isKeyPressed(RIGHT) && hero.bound.x + hero.bound.width < camera.position.x + (ScreenConstants.SCREEN_WIDTH / 2)) hero.moveRight()
+        if (Gdx.input.isKeyPressed(RIGHT) && hero.bound.x + hero.bound.width < camera.position.x + (SCREEN_WIDTH / 2)) hero.moveRight()
 
         if (enemyGroup.isCollides(hero.bound)) {
             game.screen = GameOverScreen(game)
@@ -70,10 +74,10 @@ class GameScreen(
 
         val first = enemyGroup.enemies.first()
         val last = enemyGroup.enemies.last()
-        if ((first.bound.x > camera.position.x + ScreenConstants.ENEMY_RESPAWN_BORDER + (ScreenConstants.SCREEN_WIDTH / 2) ||
-                first.bound.x + first.bound.width + ScreenConstants.ENEMY_RESPAWN_BORDER < 0) &&
-            (last.bound.x > camera.position.x + ScreenConstants.ENEMY_RESPAWN_BORDER + (ScreenConstants.SCREEN_WIDTH / 2) ||
-                last.bound.x + last.bound.width + ScreenConstants.ENEMY_RESPAWN_BORDER < 0)) {
+        if ((first.bound.x > camera.position.x + ENEMY_RESPAWN_BORDER + (SCREEN_WIDTH / 2) ||
+                first.bound.x + first.bound.width + ENEMY_RESPAWN_BORDER < 0) &&
+            (last.bound.x > camera.position.x + ENEMY_RESPAWN_BORDER + (SCREEN_WIDTH / 2) ||
+                last.bound.x + last.bound.width + ENEMY_RESPAWN_BORDER < 0)) {
             enemyGroup = EnemyGroupFactory.create(EnemyGroupFactory.GroupType.values()[Random().nextInt(EnemyGroupFactory.GroupType.values().size)])
         }
     }
