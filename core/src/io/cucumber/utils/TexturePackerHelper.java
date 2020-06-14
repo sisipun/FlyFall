9829cf249d8fc14c515b4b3f4cb92315dd078396
Alexey <sisipun@gmail.com>
2020-06-14 16:19:19 +0300

    Build apk

--- core/src/io/cucumber/utils/TexturePackerHelper.java
+++ core/src/io/cucumber/utils/TexturePackerHelper.java
@@ -1,13 +1,13 @@
 package io.cucumber.utils;
 
-import com.badlogic.gdx.tools.texturepacker.TexturePacker;
+//import com.badlogic.gdx.tools.texturepacker.TexturePacker;
 
 public class TexturePackerHelper {
 
-    public static void main(String[] args) {
-        TexturePacker.Settings settings = new TexturePacker.Settings();
-        settings.maxWidth = 4096;
-        settings.maxHeight = 4096;
-        TexturePacker.process(settings, "android/assets/images", "android/assets/atlas", "game");
-    }
+//    public static void main(String[] args) {
+//        TexturePacker.Settings settings = new TexturePacker.Settings();
+//        settings.maxWidth = 4096;
+//        settings.maxHeight = 4096;
+//        TexturePacker.process(settings, "android/assets/images", "android/assets/atlas", "game");
+//    }
 }
