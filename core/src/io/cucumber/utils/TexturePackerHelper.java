14b735b8ed2dc95e8efb479bb3b1a7e472d9bac5
Sisipun <sisipun@gmail.com>
2020-11-10 16:41:23 +0300

    Power of 2 scale to textures

--- core/src/io/cucumber/utils/TexturePackerHelper.java
+++ core/src/io/cucumber/utils/TexturePackerHelper.java
@@ -1,11 +1,15 @@
 package io.cucumber.utils;
 
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
+
 public class TexturePackerHelper {
 
-//    public static void main(String[] args) {
-//        TexturePacker.Settings settings = new TexturePacker.Settings();
-//        settings.maxWidth = 4096;
-//        settings.maxHeight = 4096;
-//        TexturePacker.process(settings, "android/assets/images", "android/assets/atlas", "game");
-//    }
+    public static void main(String[] args) {
+        TexturePacker.Settings settings = new TexturePacker.Settings();
+        settings.maxWidth = 2048;
+        settings.maxHeight = 2048;
+        settings.paddingX = 0;
+        settings.paddingY = 0;
+        TexturePacker.process(settings, "android/assets/images", "android/assets/atlas", "game");
+    }
 }
